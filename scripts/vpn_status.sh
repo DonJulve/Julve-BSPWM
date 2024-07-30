6f4ec384ba3ff36fd40b1c76fddc3b35e6df6709
DonJulve <840710@unizar.es>
2024-07-30 11:03:43 +0200

    Scripts actualizados

--- scripts/vpn_status.sh
+++ scripts/vpn_status.sh
@@ -5,5 +5,5 @@ IFACE=$(/usr/sbin/ifconfig | grep tun0 | awk '{print $1}' | tr -d ':')
 if [ "$IFACE" = "tun0" ]; then
 	echo "%{F#ffffff}  %{F#ffffff}$(/usr/sbin/ifconfig tun0 | grep "inet " | awk '{print $2}')%{u-}"
 else
-	echo "%{F#ffffff} %{u-} Disconnected"
+	echo "%{F#ffffff} %{u-} Desconectado"
 fi
