33f6a170377c942ac853ba97f6e2f744a2ab4f57
DonJulve <840710@unizar.es>
2025-09-02 13:23:20 +0200

    Mejorado interfaz de red en la polybar para detectar eth0, wlan0 o si no hay activa

--- config/polybar/scripts/ethernet_status.sh
+++ config/polybar/scripts/ethernet_status.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
 
-# echo "%{F#ffffff}  %{F#ffffff}$(/usr/sbin/ifconfig eth0 | grep "inet " | awk '{print $2}')%{u-}"
-echo "%{F#ffffff}   %{F#ffffff}$(/usr/sbin/ifconfig wlan0 | grep "inet " | awk '{print $2}')%{u-}"
+# Verificar si eth0 tiene IP
+ETH_IP=$(/usr/sbin/ifconfig eth0 2>/dev/null | grep "inet " | awk '{print $2}')
+WLAN_IP=$(/usr/sbin/ifconfig wlan0 2>/dev/null | grep "inet " | awk '{print $2}')
+
+if [ -n "$ETH_IP" ]; then
+    # Conexión por cable
+    echo "%{F#ffffff}  %{F#ffffff}$ETH_IP%{u-}"
+elif [ -n "$WLAN_IP" ]; then
+    # Conexión por WiFi
+    echo "%{F#ffffff}   %{F#ffffff}$WLAN_IP%{u-}"
+else
+    # Sin conexión
+    echo "%{F#ff0000}   Sin conexión%{u-}"
+fi
