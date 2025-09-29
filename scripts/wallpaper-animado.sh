436f2d34edaad60c2ad32c93e6647ffb0a2b33a6
DonJulve <840710@unizar.es>
2025-09-29 19:20:48 +0200

    Mejorado comando wallpaper animado

--- scripts/wallpaper-animado.sh
+++ scripts/wallpaper-animado.sh
@@ -15,15 +15,16 @@ else
 
     if [ "$monitor_count" -eq 1 ]; then
     	xwinwrap -fs -fdt -ni -b -nf -un -o 1.0 -debug -- mpv -wid WID --loop --no-audio --no-resume-playback --no-terminal --msg-level=all=no ~/Wallpapers/wallpaper.mp4 2>/dev/null &
-    elif [ "$monitor_count" -eq 2 ]; then
-	
-
-    	# Primer monitor - 1920x1080+0+0
-    	xwinwrap -g 1920x1080+0+0 -ni -s -st -sp -b -nf -ov -- mpv -wid WID --loop --no-audio --no-resume-playback --no-terminal --msg-level=all=no ~/Wallpapers/wallpaper.mp4 2>/dev/null &
-    
-    	# Segundo monitor: 1366x768+1920+0  
-    	xwinwrap -g 1366x768+1920+0 -ni -s -st -sp -b -nf -ov -- mpv -wid WID --loop --no-audio --no-resume-playback --no-terminal --msg-level=all=no ~/Wallpapers/wallpaper.mp4 2>/dev/null &
-    
+    else
+        # Varios monitores: obtener resoluciones y posiciones
+        xrandr --query | grep " connected" | while read -r line; do
+            geometry=$(echo "$line" | grep -oP '\d+x\d+\+\d+\+\d+')
+            if [ -n "$geometry" ]; then
+                xwinwrap -g "$geometry" -ni -s -st -sp -b -nf -ov -- \
+                mpv -wid WID --loop --no-audio --no-resume-playback --no-terminal \
+                --msg-level=all=no ~/Wallpapers/wallpaper.mp4 2>/dev/null &
+            fi
+        done
     fi
     
 fi
